a7954fa446f7b53a0653897eaed9476eb59d1bca
ArOk2155 <ArOk2155.1112@gmail.com>
2022-07-04 20:03:41 +0300

    Add project files.

---
+++ Lesson 4 _ 2022-07-04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_4___2022_07_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
